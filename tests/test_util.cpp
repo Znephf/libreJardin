37ca44ef8f0a7159ca303d14c4f1b365c476b3ee
Znephf <33689165+Znephf@users.noreply.github.com>
2025-06-05 17:43:49 +0200

    Add tests for util functions

--- tests/test_util.cpp
+++ tests/test_util.cpp
@@ -1,4 +1,5 @@
 #include <QtTest/QtTest>
+#include <QLocale>
 #include "../utilitaires/util.h"
 
 class TestUtil : public QObject
@@ -7,6 +8,9 @@ class TestUtil : public QObject
 private slots:
     void afficheZeros_data();
     void afficheZeros();
+    void apos_data();
+    void apos();
+    void getLocale();
 };
 
 void TestUtil::afficheZeros_data()
@@ -23,6 +27,28 @@ void TestUtil::afficheZeros()
     QFETCH(QString, input);
     QFETCH(QString, expected);
     QCOMPARE(util::afficheZeros(input), expected);
+}
+
+void TestUtil::apos_data()
+{
+    QTest::addColumn<QString>("input");
+    QTest::addColumn<QString>("expected");
+
+    QTest::newRow("no quote") << QString("Hello") << QString("Hello");
+    QTest::newRow("single quote") << QString("O'Brien") << QString("O''Brien");
+    QTest::newRow("multiple quotes") << QString("'quoted'") << QString("''quoted''");
+}
+
+void TestUtil::apos()
+{
+    QFETCH(QString, input);
+    QFETCH(QString, expected);
+    QCOMPARE(util::apos(input), expected);
+}
+
+void TestUtil::getLocale()
+{
+    QCOMPARE(util::getLocale(), QLocale::system().name());
 }
 
 QTEST_MAIN(TestUtil)
