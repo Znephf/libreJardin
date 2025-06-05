9ffa521f94cf2ce5ad260ac73442b1d1fdb07dfe
Znephf <33689165+Znephf@users.noreply.github.com>
2025-06-05 17:49:46 +0200

    Expand util tests and add coverage for background and consts

--- tests/test_util.cpp
+++ tests/test_util.cpp
@@ -1,6 +1,9 @@
 #include <QtTest/QtTest>
 #include <QLocale>
 #include "../utilitaires/util.h"
+#include "../graphic/background.h"
+#include "../consts.h"
+#include <QDir>
 
 class TestUtil : public QObject
 {
@@ -11,6 +14,8 @@ private slots:
     void apos_data();
     void apos();
     void getLocale();
+    void backgroundValues();
+    void constsValues();
 };
 
 void TestUtil::afficheZeros_data()
@@ -49,6 +54,31 @@ void TestUtil::apos()
 void TestUtil::getLocale()
 {
     QCOMPARE(util::getLocale(), QLocale::system().name());
+}
+
+void TestUtil::backgroundValues()
+{
+    background bg;
+    bg.setType(1);
+    QCOMPARE(bg.getType(), 1);
+
+    QString filePath = QDir("..").filePath("jardin_type.png");
+    bg.setPixFile(filePath);
+    QCOMPARE(bg.getPixFile(), filePath);
+    QVERIFY(bg.getBgWidth() > 0);
+    QVERIFY(bg.getBgHeight() > 0);
+}
+
+void TestUtil::constsValues()
+{
+    QCOMPARE(Consts::APP_NAME, QString("LibreJardin"));
+    QCOMPARE(Consts::SPACE_CASE, 14);
+    QCOMPARE(Consts::SPACE_SNAP, 14);
+    QCOMPARE(Consts::APP_DIR, QString("libreJardin/"));
+    QCOMPARE(Consts::SHARE_DIR, QString("/usr/share/libreJardin/"));
+    QCOMPARE(Consts::FILE_NAMEXML, QString("jardin.xml"));
+    QCOMPARE(Consts::FILE_NAMESQL, QString("jardin.sqli"));
+    QCOMPARE(Consts::DEFAULT_PLAN_IMAGE_FILE, QString("jardin_type.png"));
 }
 
 QTEST_MAIN(TestUtil)
--- tests/test_util.pro
+++ tests/test_util.pro
@@ -1,10 +1,14 @@
-QT += testlib
+QT += testlib widgets
 QT -= gui
 
 CONFIG += console c++11
 CONFIG -= app_bundle
 
 SOURCES += test_util.cpp \
-           ../utilitaires/util.cpp
+           ../utilitaires/util.cpp \
+           ../graphic/background.cpp \
+           ../consts.cpp
 
-INCLUDEPATH += ../utilitaires
+INCLUDEPATH += ../utilitaires \
+               ../graphic \
+               ..
